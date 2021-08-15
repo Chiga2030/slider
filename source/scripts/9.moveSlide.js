e54b3ad81db4f956c39c94569ffd336ad2ca5f71
Anton <chiga2030@gmail.com>
2021-08-15 17:23:49 +0300

    finish

--- source/scripts/9.moveSlide.js
+++ source/scripts/9.moveSlide.js
@@ -1,4 +1,5 @@
 let clientX;
+let deltaX;
 const classActiveEl = 'active5';
 const activeEl = () => document.querySelector(`.${classActiveEl}`);
 const getX = () => event.changedTouches[0].pageX;
@@ -9,7 +10,7 @@ const foundNum = () => parseInt(wrapper5.style.transform.replace(regExpSearchNum
 
 const curatorState = {
   curentSlide: 0,
-  transform: transform5 === 48 ? 48 * 2 : 100,
+  transform: transform5 === 48 ? 48 * 2 : transform5 === 21.8 ? 21.8 : 100,
   view: transform5 === 48 ? 'tablet' : transform5 === 100 ? 'mobile' : 'desctop',
   getCurentSlide: function() {
     return this.curentSlide;
@@ -24,84 +25,21 @@ const handleMove = () => {
   const start = getX();
 
   slider5.removeEventListener('touchmove', handleMove);
-  const deltaX = start - clientX;
+  deltaX = start - clientX;
 
-  setTimeout(() => slider5.addEventListener('touchmove', handleMove), 20);
+  setTimeout(() => slider5.addEventListener('touchmove', handleMove), 50);
+}
 
-  translate(wrapper5, +deltaX);
+
+const defineSlide = () => {
+  deltaX > 50 ? newSlideTo('left') : deltaX < -50 ? newSlideTo('right') : null;
 }
 
 const handleEnd = () => {
-  const curent = foundNum();
-  const wrapper = wrapper5;
-  const transform = curatorState.transform === 96 ? 96/2 : 100;
-  const active = 'active5';
-  let n = 0;
-  const setNewActiveEl = (n) => wrapper.getElementsByTagName('div')[n].classList.add(active);
-  const halfItems = (arr) => arr.map(item => item / 2);
-  const settings = {
-    first: [0, -50],
-    second: [-50, -150],
-    third: [-150, -250],
-    fourth: [-250, -350],
-    fifth: [-350],
-    getFirst: function() {
-      if (curatorState.view === 'tablet') {
-        return halfItems(this.first);
-      }
-      return this.first
-    },
-    getSecond: function() {
-      if (curatorState.view === 'tablet') {
-        return halfItems(this.second);
-      }
-      return this.second
-    },
-    getThird: function() {
-      if (curatorState.view === 'tablet') {
-        return halfItems(this.third);
-      }
-      return this.third
-    },
-    getFourth: function() {
-      if (curatorState.view === 'tablet') {
-        return halfItems(this.fourth);
-      }
-      return this.fourth
-    },
-    getFifth: function() {
-      if (curatorState.view === 'tablet') {
-        return halfItems(this.fifth);
-      }
-      return this.fifth
-    },
-  }
+  defineSlide()
 
-  activeEl().classList.remove(active);
-
-  if (curent > settings.getFirst()[0] || curent < settings.getFirst[0] && curent > settings.getFirst[1]) {
-    n = 0;
-  } else if (curent < settings.getSecond()[0] && curent > settings.getSecond()[1]) {
-    n = 1;
-  } else if (curent < settings.getThird()[0] && curent > settings.getThird()[1]) {
-    n = 2;
-  } else if (curent < settings.getFourth()[0] && curent > settings.getFourth()[1]) {
-    n = 3;
-  } else if (curent < settings.getFifth()[0]) {
-    n = 4;
-  }
-
-  setNewActiveEl(n);
-  newToSlide(wrapper, transform, n);
-  curatorState.setCurentSlide(n);
   setTimeout(() => wrapper5.classList.remove('willChange'), 2000)
 };
-
-
-const translate = (wrapper, value) => {
-  const curent = curatorState.getCurentSlide() * -(curatorState.view === 'tablet' ? curatorState.transform / 2 : curatorState.transform);
-  wrapper.style.transform = `translateX(${curent + value}vw)`;
-}
 
 
 const newToSlide = (wrapper, transform, num) => {
@@ -112,7 +50,7 @@ const newToSlide = (wrapper, transform, num) => {
 const newSlideTo = (direction) => {
   const wrapper = wrapper5;
   const active = 'active5';
-  const transform = curatorState.transform === 96 ? 96/2 : 100;
+  const transform = curatorState.transform === 96 ? 96/2 : curatorState.view === 'desctop' ? curatorState.transform : 100;
   let n = curatorState.getCurentSlide();
 
   wrapper.children[n].classList.remove(active)
@@ -129,7 +67,7 @@ const newSlideTo = (direction) => {
     } else {
       n = n + 1;
     }
-  } else {
+  } else if (direction === 'left') {
     if(n > 0) {
       wrapper.children[n-1].classList.add(active)
       curatorState.setCurentSlide(n-1)
@@ -138,7 +76,7 @@ const newSlideTo = (direction) => {
       curatorState.setCurentSlide(wrapper.children.length-1)
     }
 
-    if(n <= 1) {
+    if(n < 1) {
       n = wrapper.children.length-1;
     } else {
       n = n - 1;
@@ -151,6 +89,7 @@ const newSlideTo = (direction) => {
 
 wrapper5.addEventListener('touchstart', function(e) {
   wrapper5.classList.add('willChange');
+  deltaX = 0;
   clientX = e.touches[0].clientX;
 }, false);
 wrapper5.addEventListener('touchmove', handleMove);
